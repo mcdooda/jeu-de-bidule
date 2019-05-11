2832ac5c31502c4b4cf8293442af87267a71493d
mcdooda <nicolas.dod@gmail.com>
2019-05-11 20:28:56 +0200

    Added interlude between levels

--- 0_common/loader/loader.go
+++ 0_common/loader/loader.go
@@ -121,3 +121,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "interlude-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/0_common/interlude/interlude.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
