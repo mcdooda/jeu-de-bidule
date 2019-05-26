d83340d8d99f37ea9574231eb2527ea3e0a240b7
mcdooda <nicolas.dod@gmail.com>
2019-05-26 14:58:08 +0200

    Improved level loading flow

---
+++ 00_common/loader/level-loader.go
@@ -0,0 +1,195 @@
+components {
+  id: "level-loader"
+  component: "/00_common/loader/level-loader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "feed-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/04_feed/level/4_feed.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "watch-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/11_watch/level/11_watch.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "pet-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/02_pet/level/2_pet.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "pickup-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/06_pickup/level/6_pickup.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "throw-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/07_throw/level/7_throw.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "fly-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/09_fly/level/9_fly.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "main-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/01_main/level/1_level.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "clean-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/05_clean/level/5_clean.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "chase-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/08_chase/level/8_chase.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "find-level-proxy"
+  type: "collectionproxy"
+  data: "collection: \"/10_find/level/10_find.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
