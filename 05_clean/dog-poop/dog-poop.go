1b76de7fbd132c36964af4d88f48ee3943e948cd
mcdooda <nicolas.dod@gmail.com>
2019-05-28 23:30:12 +0200

    Tweaked the clean hard mode

--- 05_clean/dog-poop/dog-poop.go
+++ 05_clean/dog-poop/dog-poop.go
@@ -1,44 +1,14 @@
-components {
-  id: "robot-vacuum"
-  component: "/05_clean/robot-vacuum/robot-vacuum.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-components {
-  id: "smoke"
-  component: "/05_clean/robot-vacuum/smoke.particlefx"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/00_common/assets/robot-vacuum/robot-vacuum-atlas.atlas\"\n"
-  "default_animation: \"robot-vacuum\"\n"
+  data: "tile_set: \"/00_common/assets/dog-poop/dog-poop-atlas.atlas\"\n"
+  "default_animation: \"dog-poop\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
     x: 0.0
-    y: 0.0
+    y: 6.0
     z: 0.0
   }
   rotation {
@@ -56,9 +26,8 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"robot-vacuum\"\n"
-  "mask: \"dog-fur\"\n"
-  "mask: \"dog-poop\"\n"
+  "group: \"dog-poop\"\n"
+  "mask: \"robot-vacuum\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
@@ -76,7 +45,7 @@ embedded_components {
   "    index: 0\n"
   "    count: 1\n"
   "  }\n"
-  "  data: 50.0\n"
+  "  data: 12.5\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
